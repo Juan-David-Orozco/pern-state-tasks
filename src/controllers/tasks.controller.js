68b8b4ea8e23322a2c8efca4dc217f416adc59d1
Juan-David-Orozco <ing.ele2018@gmail.com>
2022-05-12 19:05:08 -0500

    definiendo CRUD y middleware error

--- src/controllers/tasks.controller.js
+++ src/controllers/tasks.controller.js
@@ -1,33 +1,75 @@
-//const pool = require('../db')
+const pool = require('../db')
 
 const getAllTasks = async (req, res, next) => {
-  res.send('Retornando lista tareas');
-  /*
   try {
     const allTasks = await pool.query("SELECT * FROM task");
     res.json(allTasks.rows);
   } catch (error) {
     next(error);
+    //console.log(error.message)
   }
-  */
 };
 
-const getTask = (req, res) => {
-  res.send('Retornando tarea');
-}
+const getTask = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const result = await pool.query("SELECT * FROM task WHERE id = $1", [id]);
+    if (result.rows.length === 0)
+      return res.status(404).json({ message: "Task not found" });
+    res.json(result.rows[0]);
+  } catch (error) {
+    next(error);
+    //console.log(error.message)
+  }
+};
 
 const createTask = async (req, res, next) => {
-  res.send('Creando tarea')
+  try {
+    const { title, description } = req.body;
+    const newTask = await pool.query(
+      "INSERT INTO task (title, description) VALUES($1, $2) RETURNING *",
+      [title, description]
+    );
+    res.json(newTask.rows[0]);
+  } catch (error) {
+    next(error);
+    //console.log(error.message)
+    //res.json({error: error.message})
+  }
 };
 
-const updateTask = async (req, res) => {
-  res.send('Actualizando tarea')
+const updateTask = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const { title, description } = req.body;
+
+    const result = await pool.query(
+      "UPDATE task SET title = $1, description = $2 WHERE id = $3 RETURNING *",
+      [title, description, id]
+    );
+
+    if (result.rows.length === 0)
+      return res.status(404).json({ message: "Task not found" });
+
+    return res.json(result.rows[0]);
+  } catch (error) {
+    next(error);
+    //console.log(error.message)
+  }
 };
 
-const deleteTask = async (req, res) => {
-  res.send('Eliminando tarea')
+const deleteTask = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const result = await pool.query("DELETE FROM task WHERE id = $1", [id]);
+    if (result.rowCount === 0)
+      return res.status(404).json({ message: "Task not found" });
+    return res.sendStatus(204);
+  } catch (error) {
+    next(error);
+    //console.log(error.message)
+  }
 };
-
 
 module.exports = {
   createTask,
--- src/index.js
+++ src/index.js
@@ -7,9 +7,19 @@ const router = require("./routes/tasks.routes");
 const app = express();
 
 app.use(morgan("dev"));
+app.use(express.json());
+
 
 // Routes
-app.use(router);
+app.use(router)
+
+// handling errors
+app.use((err, req, res, next) => {
+  return res.status(500).json({
+    status: "error",
+    message: err.message,
+  });
+});
 
 app.listen(5000);
 console.log("Server on port 5000");
