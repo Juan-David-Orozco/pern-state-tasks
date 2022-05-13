c381a6bc0b01db239c37c3ab8dc9f7195cdaa5e0
Juan-David-Orozco <ing.ele2018@gmail.com>
2022-05-12 19:28:51 -0500

    configurando variables de entorno DB y modulo cors

---
+++ src/config.js
@@ -0,0 +1,12 @@
+const {config} = require('dotenv')
+config()
+
+module.exports = {
+  db: {
+    user: process.env.DB_USER || "postgres",
+    password: process.env.DB_PASSWORD || "",
+    host: process.env.DB_HOST || "localhost",
+    port: process.env.DB_PORT || 5432,
+    database: process.env.DB_DATABASE || "tasksdb",
+  },
+};
--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 const express = require("express");
 const morgan = require("morgan");
-//const cors = require("cors");
-
+const cors = require("cors");
 const router = require("./routes/tasks.routes");
 
 const app = express();
 
+app.use(cors())
 app.use(morgan("dev"));
 app.use(express.json());
-
 
 // Routes
 app.use(router)
